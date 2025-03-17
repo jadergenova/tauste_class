d39e3df07d7bd5314cdee46ef2f0f2c015ab5b46
jadergenova <jadergenova@gmail.com>
2025-03-17 20:41:36 -0300

    Comecei o projeto agora

--- tauste2/flutter_application_1/lib/main.dart
+++ tauste2/flutter_application_1/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -9,17 +9,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: Center(
-          child: Text(
-            'Uber',
-            style: TextStyle(fontSize: 80, color: Colors.white),
-          ),
-        ),
-      ),
-    );
+    return MaterialApp();
   }
 }
