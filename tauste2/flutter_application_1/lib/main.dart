c5c43b666e9e2e4bff1ce2e2430ab3ab162d9823
Tauste <jadergenova@gmail.com>
2025-03-17 19:50:26 -0300

    Hello World Tauste

---
+++ tauste2/flutter_application_1/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Text(
+            'Uber',
+            style: TextStyle(fontSize: 80, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
