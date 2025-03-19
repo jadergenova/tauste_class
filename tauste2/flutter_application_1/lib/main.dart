60f4cd2a8d8c7a3eddee4479fdcf486aedc48341
jadergenova <jadergenova@gmail.com>
2025-03-19 18:09:22 -0300

    aula1703

--- tauste2/flutter_application_1/lib/main.dart
+++ tauste2/flutter_application_1/lib/main.dart
@@ -4,11 +4,62 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: 'Supermercado',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(),
+      home: iniciarComponentePrincipalDoApp(),
+    );
+  }
+
+  Widget iniciarComponentePrincipalDoApp() {
+    return Scaffold(
+      drawer: Drawer(
+        child: ListView(
+          children: const [
+            Text(
+              'Departamentos',
+              style: TextStyle(
+                color: Colors.indigo,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(),
+      body: const Center(child: Text('Hello World')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   items: const [
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.home),
+      //       label: 'Home',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.shopping_cart),
+      //       label: 'Carrinho',
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.person),
+      //       label: 'Perfil',
+      //     ),
+      //   ],
+      // ),
+    );
   }
 }
